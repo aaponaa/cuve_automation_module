9797ffcfd6d692d8604d16a798c6470653333617
Benjamin Maas <aaponaa18@gmail.com>
2025-06-13 10:14:39 +0200

    topic showing again in setting

--- Aqua-Link/src/web_routes.cpp
+++ Aqua-Link/src/web_routes.cpp
@@ -148,6 +148,17 @@ void initWebServer() {
     delay(500); 
     ESP.restart();
   }, handleFirmwareUpload);
+  server.on("/topics_config", HTTP_GET, []() {
+    String json = "[";
+    for (int i = 0; i < NUM_TOPICS; ++i) {
+      if (i > 0) json += ",";
+      json += "{\"topic\":\"" + tank_name + "/" + topicOptions[i].topic_suffix + "\",";
+      json += "\"suffix\":\"" + String(topicOptions[i].topic_suffix) + "\",";
+      json += "\"enabled\":" + String(topicOptions[i].enabled ? "true" : "false") + "}";
+    }
+    json += "]";
+    server.send(200, "application/json", json);
+  });
   server.begin();
   Serial.println("✅ Web server started");
 }
